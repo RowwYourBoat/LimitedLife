80b0bffc943b2bdcd01e846b72c7eb24fd47f214
icallhacks <rowandoorneweerd9@gmail.com>
2023-03-07 17:04:02 +0100

    Enchantment Nerfing, Update Checker and Metrics

--- src/main/java/me/rowyourboat/limitedlife/LimitedLife.java
+++ src/main/java/me/rowyourboat/limitedlife/LimitedLife.java
@@ -3,12 +3,11 @@ package me.rowyourboat.limitedlife;
 import me.rowyourboat.limitedlife.commands.MainCommandExecutor;
 import me.rowyourboat.limitedlife.commands.MainTabCompleter;
 import me.rowyourboat.limitedlife.data.SaveHandler;
-import me.rowyourboat.limitedlife.listeners.ChatFormat;
-import me.rowyourboat.limitedlife.listeners.InventoryEvents;
-import me.rowyourboat.limitedlife.listeners.PlayerDeathEvents;
-import me.rowyourboat.limitedlife.listeners.PlayerJoinEvents;
+import me.rowyourboat.limitedlife.listeners.*;
 import me.rowyourboat.limitedlife.scoreboard.TeamHandler;
 import me.rowyourboat.limitedlife.util.CustomRecipes;
+import me.rowyourboat.limitedlife.util.bStatsCode;
+import org.bstats.bukkit.Metrics;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.command.CommandSender;
@@ -27,6 +26,7 @@ public final class LimitedLife extends JavaPlugin {
 
     @Override
     public void onEnable() {
+        int configVersion = 1;
         plugin = this;
         globalTimerActive = false;
 
@@ -44,6 +44,8 @@ public final class LimitedLife extends JavaPlugin {
         Bukkit.getPluginManager().registerEvents(new PlayerJoinEvents(), plugin);
         Bukkit.getPluginManager().registerEvents(new InventoryEvents(), plugin);
         Bukkit.getPluginManager().registerEvents(new ChatFormat(), plugin);
+        Bukkit.getPluginManager().registerEvents(new EnchantmentLimitations(), plugin);
+
 
         plugin.saveDefaultConfig();
         plugin.getServer().getConsoleSender().sendMessage(ChatColor.GREEN + "[LimitedLife] The plugin has been loaded!"
@@ -53,6 +55,24 @@ public final class LimitedLife extends JavaPlugin {
         );
 
         Bukkit.getOnlinePlayers().forEach(CustomRecipes::grant);
+
+
+
+        new Metrics(plugin, bStatsCode.get());
+
+        new UpdateChecker(plugin, 0).getVersion(version -> {
+            if (this.getDescription().getVersion().equals(version)) {
+                getLogger().info("The plugin is up to date!");
+            } else {
+                getLogger().severe("There is a newer version available!  Running: " + this.getDescription().getVersion() + "  Newest: " + version);
+                getLogger().severe("You may download it here: https://modrinth.com/plugin/limited-life");
+            }
+        });
+
+        if (getConfig().getInt("config-version") != configVersion) {
+            getLogger().warning("Your configuration file is " + (configVersion - getConfig().getInt("config-version")) + " version(s) behind!");
+            getLogger().warning("To be able to access the newly added settings, please delete the current config.yml file and restart/reload the server!");
+        }
     }
 
     @Override
